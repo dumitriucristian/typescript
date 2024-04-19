a8f02ab7da98531ce2983009d4d1a55067d5869e
cristi <dumitriucristian@yahoo.com>
2024-04-19 10:47:37 +0200

    first commit

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var character = 'Volvo is not a car';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ trips-and-tricks.md
@@ -0,0 +1,8 @@
+convert ts to js
+tsc sandbox.ts
+
+convert and watch
+tsc sandbox.ts -w
+
+initialize a compiler
+tsc --init
