570c42e7eb3983a38d1fa6028e7fd6052645896c
cristi <dumitriucristian@yahoo.com>
2024-04-19 15:38:46 +0200

    first class

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,4 +1,18 @@
 "use strict";
+class Invoice {
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+        this.client = client;
+    }
+    ;
+    format() {
+        return `This invoice is for: ${this.client} representing ${this.details} with cost of ${this.amount} dolars`;
+    }
+}
+let firstInvoice = new Invoice("janel", "Web Services", 100);
+console.log(firstInvoice.format());
 const form = document.querySelector(".new-item-form");
 const type = document.getElementById("type");
 const toFrom = document.getElementById("tofrom");
@@ -6,6 +20,8 @@ const details = document.getElementById("details");
 const amount = document.getElementById("amount");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let secondInvoice = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    console.log(secondInvoice.format());
     console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
     //console.log(details.value);
     // console.log(amount.value);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,25 @@
+
+
+class Invoice {
+
+  constructor( 
+      readonly client: string,
+      public details: string,
+      public amount: number 
+
+    ){
+      this.client = client;
+    };
+
+  format():string {
+    return  `This invoice is for: ${this.client} representing ${this.details} with cost of ${this.amount} dolars`;
+  }
+
+}
+
+let firstInvoice  = new Invoice("janel", "Web Services", 100);
+console.log(firstInvoice.format());
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
 const type = document.getElementById("type") as HTMLSelectElement;
@@ -7,11 +29,14 @@ const amount = document.getElementById("amount") as HTMLInputElement;
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
+  let secondInvoice  = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+  console.log(secondInvoice.format());
   console.log(
     type.value,
     toFrom.value,
     details.value,
     amount.valueAsNumber
+  
     
   );
 
